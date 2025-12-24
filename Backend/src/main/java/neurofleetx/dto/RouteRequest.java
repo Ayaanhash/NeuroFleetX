f16e71ebe2bcc12f74f5525fee8ed65cc8774410
Srijita <srijitakook@gmail.com>
2025-12-24 18:55:42 +0530

    dtos for request and response added

---
+++ Backend/src/main/java/neurofleetx/dto/RouteRequest.java
@@ -0,0 +1,31 @@
+package com.neurofleetx.dto;
+
+public class RouteRequest {
+
+    private String source;
+    private String destination;
+
+    // Default constructor (important for Spring)
+    public RouteRequest() {}
+
+    public RouteRequest(String source, String destination) {
+        this.source = source;
+        this.destination = destination;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+}
